8f20578e7c4d238d8b8b6aa207a6131c382f6955
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-02-08 23:23:44 +0900

    setup mongoose

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = require('./server/app.js');
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`App running on http://127.0.0.1:${PORT}...`);
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DATABASE connection successful!'));
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`App running on http://127.0.0.1:${port}...`);
 });
