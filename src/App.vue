840de6b1856ace8ac25cfe2baf671b64e922990b
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-02-09 18:57:53 +0900

    update front end

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Final Fantasy Browser</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <v-list dense rounded>
+        <v-list-item
+          v-for="platform in platforms"
+          :key="platform"
+          @click="onPlatformClick(platform)"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ platform }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <v-col v-for="game in games" :key="game" cols="4">
+            <v-img :src="game.img" height="400"></v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  mounted() {
+    console.log('this.getGames() running...');
+    this.getGames();
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({ drawer: null }),
+
+  computed: {
+    games() {
+      return this.$store.state.displayedGames;
+    },
+    platforms() {
+      return this.$store.state.platforms;
+    },
+  },
+
+  methods: {
+    getGames() {
+      this.$store.dispatch('loadGames');
+    },
+    onPlatformClick(platform) {
+      console.log('onPlatformClick running....', platform);
+      if (platform === 'All') {
+        this.$store.dispatch('clearFilter');
+      } else {
+        this.$store.dispatch('setFilter', platform);
+      }
+    },
+  },
+};
+</script>
--- src/store.js
+++ src/store.js
@@ -1,11 +1,64 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+const axios = require('axios');
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  getters: {},
+  state: {
+    allGames: [],
+    displayedGames: [],
+    platforms: [],
+  },
+  mutations: {
+    setAllGames(state, payload) {
+      state.allGames = payload;
+    },
+    setDisplayedGames(state, payload) {
+      state.displayedGames = payload;
+    },
+    setPlatforms(state, payload) {
+      state.platforms = payload;
+    },
+  },
+  actions: {
+    async loadGames() {
+      try {
+        const { data: games } = await axios.get('api/games');
+
+        const items = games.data.games;
+        const platforms = ['All'];
+
+        for (let index = 0; index < items.length; index++) {
+          const game = items[index];
+          if (!platforms.includes(game.platforme))
+            platforms.push(game.platforme);
+        }
+
+        this.commit('setAllGames', items);
+        this.commit('setDisplayedGames', items);
+        this.commit('setPlatforms', platforms);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    clearFilter({ state }) {
+      this.commit('setDisplayedGames', state.allGames);
+    },
+    setFilter({ state }, platform) {
+      const displayedGames = [];
+      for (let index = 0; index < state.allGames.length; index++) {
+        const element = state.allGames[index];
+        if (element.platforme === platform) {
+          displayedGames.push(element);
+        }
+      }
+      this.commit('setDisplayedGames', displayedGames);
+    },
+  },
+  getters: {
+    getGames(state) {
+      return state.games;
+    },
+  },
 });
