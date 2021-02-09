8007ca09d7a107b6aa6dc8cdf94223ed33ded1ae
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-02-09 14:13:20 +0900

    show front end on heroku

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const gameRouter = require('../routes/gameRoutes');
 
 const app = express();
@@ -7,5 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/games', gameRouter);
+app.use(express.static(path.resolve(__dirname, '..', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
+});
 
 module.exports = app;
